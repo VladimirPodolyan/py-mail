2bb8456fc4dd14153f1fb8240c16a1b87bbf5876
PVladimir <vladimir.podolyan@techops.ru>
2019-06-18 22:18:30 +0300

    initial commit - add License & fix url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="gmail client for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/VladimirPodolyan/pymail",
+    url="https://github.com/VladimirPodolyan/py-mail",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
