0cf36931ea847a4090a7bc9a42ed0f3843b5437d
PVladimir <vladimir.podolyan@techops.ru>
2019-06-18 21:52:43 +0300

    initial commit - change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymail",
+    name="py-mail",
     version="0.0.1",
     author="PVladimir",
     author_email="vladimir.podolyan64@gmail.com",
